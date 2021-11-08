3a8c63d7c82a8f258dd94843d68349cf3e54281b
Olena Aksonova <90653628+AksonovaOlena@users.noreply.github.com>
2021-11-08 23:47:06 +0200

    Add files via upload

---
+++ random_words.py
@@ -0,0 +1,11 @@
+import random
+animals=['dog','puppy','cat','kitten','rabbit','hamster','chinchilla','guenia pig','mouse','rat']
+birds=['sparrow','penguin','dove','swan','owl','crane']
+sound=['squeak','moo','roar','tweet','bark','purr','quack']
+option1=random.choice(animals)
+option2=random.choice(birds)
+option3=random.choice(sound)
+print(option1+' and '+option2+' are '+option3)
+input()
+
+
